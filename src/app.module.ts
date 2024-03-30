3cdf38b741bb83e8a03b9240bafb9e9e53408094
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-03-30 18:14:47 +0100

    add roles module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AuthModule } from './modules/auth/auth.module';
+import { RolesModule } from './modules/roles/roles.module';
 
 @Module({
-  imports: [AuthModule],
+  imports: [AuthModule, RolesModule],
 })
 export class AppModule {}
