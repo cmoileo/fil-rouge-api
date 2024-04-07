4388e92978b2d275f4e1e2ce95242e2f2e6439ab
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-07 20:07:50 +0200

    handle create task

--- src/modules/tasks/tasks.controller.ts
+++ src/modules/tasks/tasks.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpException, Post, Req, UseGuards } from "@nestjs/common";
+import { TasksService } from './tasks.service';
+import { JwtAuthGuard } from '../guards/verify-jwt.guard';
+import { CreateTaskDto } from './dto/create-task.dto';
 
+@UseGuards(JwtAuthGuard)
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
+  @Post('/create')
+  async createTask(
+    @Body() body: CreateTaskDto,
+    @Req() req: any,
+  ): Promise<boolean | HttpException> {
+    const agency_id = req.userEmail;
+    return this.tasksService.createTask(agency_id, body);
+  }
+}
--- src/modules/tasks/tasks.service.ts
+++ src/modules/tasks/tasks.service.ts
@@ -1,4 +1,18 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { CreateTaskService } from './service/create-task.service';
 
 @Injectable()
-export class TasksService {}
+export class TasksService {
+  constructor(private readonly prisma: PrismaClient) {
+    this.prisma = new PrismaClient();
+  }
+
+  async createTask(
+    agency_id: string,
+    body: CreateTaskDto,
+  ): Promise<boolean | HttpException> {
+    return new CreateTaskService(this.prisma, agency_id, body).createTask();
+  }
+}
