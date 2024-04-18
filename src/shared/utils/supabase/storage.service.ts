77a90fafe7358fa74417f9bd1991d1cd75f8f8f3
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-18 10:00:21 +0200

    handle compress file before upload

--- src/shared/utils/supabase/storage.service.ts
+++ src/shared/utils/supabase/storage.service.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import { extname } from 'path';
-import { fromBlob } from 'image-resize-compress';
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sharp = require('sharp');
 
 class StorageService {
   private supabase = createClient(
@@ -9,32 +10,33 @@ class StorageService {
   );
 
   async uploadFile(file: any) {
-    // const compressedBuffer = await this.compressFile(file);
-
+    let compressedBuffer = file.buffer;
+    while (compressedBuffer.length > 512000) {
+      compressedBuffer = await this.compressFile(compressedBuffer);
+    }
     const { data, error } = await this.supabase.storage
       .from('storage')
-      .upload(`${Date.now()}${extname(file.originalname)}`, file);
+      .upload(`${Date.now()}${extname(file.originalname)}`, compressedBuffer);
     if (error) {
       throw new Error(error.message);
     }
     return data;
   }
 
-  // private async compressFile(buffer: File): Promise<Blob> {
-  //   const quality = 80;
-  //   const width = 0;
-  //   const height = 0;
-  //   const format = 'webp';
-  //
-  //   const compressedBuffer = await fromBlob(
-  //     buffer,
-  //     quality,
-  //     width,
-  //     height,
-  //     format,
-  //   );
-  //   return compressedBuffer;
-  // }
+  private async compressFile(buffer: Buffer): Promise<Buffer> {
+    console.log('compressing...');
+    const maxWidth = 1080;
+    const maxHeight = 1080;
+    const compressedBuffer = await sharp(buffer)
+      .resize(maxWidth, maxHeight, {
+        fit: 'inside',
+        withoutEnlargement: true,
+      })
+      .webp({ quality: 80 })
+      .toBuffer();
+    console.log(compressedBuffer.length);
+    return compressedBuffer;
+  }
 }
 
 const storageService = new StorageService();
