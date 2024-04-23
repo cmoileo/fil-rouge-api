b0291ff79b5fa4b9ab1272da341a40e7da2076be
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-23 16:59:28 +0200

    add change role service'

--- src/modules/account/account.controller.ts
+++ src/modules/account/account.controller.ts
@@ -9,9 +9,11 @@ import {
 import { JwtAuthGuard } from '../guards/verify-jwt.guard';
 import { ChangeRoleDto } from './dto/change-role.dto';
 import { RoleEnum } from '../../shared/enum/role/role.enum';
+import { AccountService } from './account.service';
 
 @Controller('account')
 export class AccountController {
+  constructor(private readonly accountService: AccountService) {}
   @Post('change-role/:role')
   @UseGuards(JwtAuthGuard)
   async changeRole(
@@ -20,6 +22,6 @@ export class AccountController {
   ): Promise<string | HttpException> {
     const user_email = req.userEmail;
     const role: RoleEnum = req.params.role;
-    return 'OWNER';
+    return await this.accountService.changeRole(user_email, role, body);
   }
 }
