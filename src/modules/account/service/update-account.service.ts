4f838de5dd901653514ee9dae85417a98c422a91
cmoileo <106542483+cmoileo@users.noreply.github.com>
2024-04-23 20:40:57 +0200

    add profile picture to users

---
+++ src/modules/account/service/update-account.service.ts
@@ -0,0 +1,48 @@
+import { UpdateAccountDto } from '../dto/update-account.dto';
+import { AccountType } from '../../../shared/types/account/account.type';
+import { HttpException } from '@nestjs/common';
+import { PrismaClient } from '@prisma/client';
+import storageService from '../../../shared/utils/supabase/storage.service';
+import { extname } from 'path';
+
+export class UpdateAccountService {
+  constructor(
+    private readonly prisma: PrismaClient,
+    private readonly user_email: string,
+    private readonly body: UpdateAccountDto,
+  ) {}
+  async execute(): Promise<AccountType | HttpException> {
+    try {
+      const user = await this.prisma.user.findUnique({
+        where: {
+          email: this.user_email,
+        },
+      });
+      if (!user) {
+        return new HttpException('User not found', 404);
+      }
+      let imageUrl: { name: string; image_url: string } | null;
+      const image_key = `${Date.now()}${extname(this.body.avatar.originalname)}`;
+      if (image_key !== user.profile_picture_key) {
+        imageUrl = await storageService.uploadFile(this.body.avatar);
+      }
+      const updatedUser: AccountType = await this.prisma.user.update({
+        where: {
+          email: this.user_email,
+        },
+        data: {
+          email: this.body.email,
+          firstname: this.body.firstname,
+          lastname: this.body.lastname,
+          profile_picture_url: imageUrl
+            ? imageUrl.image_url
+            : user.profile_picture_url,
+          profile_picture_key: imageUrl ? image_key : user.profile_picture_key,
+        },
+      });
+      return updatedUser;
+    } catch (error) {
+      return new HttpException(error.message, error.status);
+    }
+  }
+}
