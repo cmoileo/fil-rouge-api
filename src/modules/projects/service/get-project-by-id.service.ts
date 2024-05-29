0f05105812021c3613dc1d9602aeba67a7913e14
cmoileo <leo.fezard33@gmail.com>
2024-05-29 07:36:43 +0200

    return signed profil picture url when returns projects

--- src/modules/projects/service/get-project-by-id.service.ts
+++ src/modules/projects/service/get-project-by-id.service.ts
@@ -1,5 +1,6 @@
 import { PrismaClient, Project } from '@prisma/client';
 import { HttpException } from '@nestjs/common';
+import storageService from '../../../shared/utils/supabase/storage.service';
 
 export class GetProjectByIdService {
   constructor(
@@ -26,6 +27,23 @@ export class GetProjectByIdService {
       });
       if (!project) {
         throw new HttpException('Project not found', 404);
+      }
+      for (const task of project.tasks) {
+        for (const taskUser of task.task_users) {
+          if (taskUser.employe.profile_picture_url) {
+            try {
+              const signedUrl = await storageService.getSignedUrl(
+                taskUser.employe.profile_picture_url,
+              );
+              taskUser.employe.profile_picture_url = signedUrl;
+            } catch (error) {
+              console.error('Error getting signed URL:', error);
+              taskUser.employe.profile_picture_url = null;
+            }
+          } else {
+            taskUser.employe.profile_picture_url = null;
+          }
+        }
       }
       return project;
     } catch (error) {
